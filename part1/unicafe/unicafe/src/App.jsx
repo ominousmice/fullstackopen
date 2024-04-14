ce2745f83a6174ca53fdaca05fef7706915148ed
ominousmice <nicoleynicoleal@gmail.com>
2024-04-14 14:01:04 -0300

    exercise 1.11

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -20,12 +20,16 @@ const Statistics = ({ good, neutral, bad, total, avg, positive }) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <StatisticLine text="good" value={good}/>
-      <StatisticLine text="neutral" value={neutral}/>
-      <StatisticLine text="bad" value={bad}/>
-      <StatisticLine text="total" value={total}/>
-      <StatisticLine text="average" value={avg}/>
-      <StatisticLine text="positive" value={positive}/>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good}/>
+          <StatisticLine text="neutral" value={neutral}/>
+          <StatisticLine text="bad" value={bad}/>
+          <StatisticLine text="total" value={total}/>
+          <StatisticLine text="average" value={avg}/>
+          <StatisticLine text="positive" value={positive}/>
+        </tbody>
+      </table>
     </div>
   )
 }
@@ -33,9 +37,19 @@ const Statistics = ({ good, neutral, bad, total, avg, positive }) => {
 // StatisticLine component
 const StatisticLine = ({text, value}) => {
   if (text === "positive") {
-    return <p>{text} {value}%</p>
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value}%</td>
+      </tr>
+  )
   }
-  return <p>{text} {value}</p>
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
 }
 
 const App = () => {
