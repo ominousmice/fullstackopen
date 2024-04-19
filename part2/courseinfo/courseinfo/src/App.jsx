231cb5587396623f7f85796ecbc683e92ab7f823
ominousmice <nicoleynicoleal@gmail.com>
2024-04-19 19:00:29 -0300

    exercises 2.1 and 2.2

---
+++ part2/courseinfo/courseinfo/src/App.jsx
@@ -0,0 +1,50 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
+    </div>
+  )
+}
+
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Content = ({ parts }) => {
+  return parts.map(eachPart => <Part part={eachPart} key={eachPart.id}/>)
+}
+
+const Part = ({ part }) => 
+<p>
+  {part.name} {part.exercises}
+</p>
+
+const Total = ({ sum }) => <p><strong>Total of {sum} exercises</strong></p>
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
