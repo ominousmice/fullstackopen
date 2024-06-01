cde8bb106e8abddbfa764da0490e176de864bd82
ominousmice <nicoleynicoleal@gmail.com>
2024-06-01 19:13:17 -0300

    ex 4.4

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blogs) => {
+    const likes = blogs.map(eachBlog => eachBlog.likes)
+    
+    const sum = likes.reduce((sum, likes) => {
+        return sum + likes
+    }, 0)
+    
+    return sum
+}
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
