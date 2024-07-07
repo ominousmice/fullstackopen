84c82849178e86ca38d8825f8c4ec0e62ff294d7
ominousmice <nicoleynicoleal@gmail.com>
2024-07-07 15:00:16 -0300

    ex 5.6 & 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  if (showDetails) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={toggleShowDetails}>hide</button>
+        <br></br>{blog.url}
+        <br></br>{blog.likes} <button>like</button>
+        <br></br>{blog.user.name}
+      </div>  
+    )
+  }
+  return (
+  <div style={blogStyle}>
     {blog.title} {blog.author}
+    <button onClick={toggleShowDetails}>view</button>
   </div>  
-)
+)}
 
 export default Blog
